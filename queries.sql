5d29f15076545bb1be7e4cd620b0d66ff37fbfd8
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-08-21 18:55:29 -0400

    Add transactions and queries

--- queries.sql
+++ queries.sql
@@ -13,3 +13,58 @@ SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+
+BEGIN;
+
+UPDATE animals SET species = 'unspecified';
+
+SELECT * FROM animals;
+
+ROLLBACK;
+
+SELECT * FROM animals;
+
+
+BEGIN;
+
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+SELECT * FROM animals;
+
+COMMIT;
+
+SELECT * FROM animals;
+
+
+BEGIN;
+
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT my_savepoint;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO my_savepoint;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+COMMIT;
+
+SELECT * FROM animals;
+
+
+
+SELECT COUNT(*) FROM animals;
+
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+
+SELECT AVG(weight_kg) FROM animals;
+
+
+SELECT neutered, AVG(escape_attempts) AS average_escape_attempts FROM animals GROUP BY neutered;
+
+
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
