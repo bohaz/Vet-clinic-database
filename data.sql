4d7e8d50cc106b0cc6b9368fe5405d4e73122f0b
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-08-21 18:47:07 -0400

    Add new data to data.sql

--- data.sql
+++ data.sql
@@ -1,5 +1,8 @@
-INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES
-('Agumon', '2020-02-03', 10.23, TRUE, 0),
-('Gabumon', '2018-11-15', 8, TRUE, 2),
-('Pikachu', '2021-01-07', 15.04, FALSE, 1),
-('Devimon', '2017-05-12', 11, TRUE, 5);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species) VALUES
+('Charmander', '2020-02-08', -11, FALSE, 0, NULL),
+('Plantmon', '2021-11-15', -5.7, TRUE, 2, NULL),
+('Squirtle', '1993-04-02', -12.13, FALSE, 3, NULL),
+('Angemon', '2005-06-12', -45, TRUE, 1, NULL),
+('Boarmon', '2005-06-07', 20.4, TRUE, 7, NULL),
+('Blossom', '1998-10-13', 17, TRUE, 3, NULL),
+('Ditto', '2022-05-14', 22, TRUE, 4, NULL);
